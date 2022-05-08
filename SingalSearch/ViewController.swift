9439020aeba222645929b31f5fc9aed25a249550
Akihiro Inui <mail@akihiroinui.com>
2022-05-08 16:57:21 +0200

    Initial Commit

---
+++ SingalSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingalSearch
+//
+//  Created by Akihiro Inui on 08/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
